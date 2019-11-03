e684b15066d23e57486352c7f252b0d3d5679341
liulong <706750966@qq.com>
2019-11-03 17:01:54 +0800

    微服务

---
+++ hello-apache-dubbo-consumer/src/main/java/com/funtl/apache/dubbo/consumer/ConsumeApplication.java
@@ -0,0 +1,11 @@
+package com.funtl.apache.dubbo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumeApplication.class,args);
+    }
+}
---
+++ hello-apache-dubbo-provider/hello-apache-dubbo-provider-api/src/main/java/com/funtl/apache/dubbo/provider/api/EchoService.java
@@ -0,0 +1,5 @@
+package com.funtl.apache.dubbo.provider.api;
+
+public interface EchoService {
+    String echo(String string);
+}
